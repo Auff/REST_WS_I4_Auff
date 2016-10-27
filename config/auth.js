f5a0fdb8a6d2896b85a77e846f23034f1591dc1b
Adrien Auffredou <adrien.auffredou@gmail.com>
2016-10-27 16:14:00 +0200

    callback change
    https://floating-inlet-92048.herokuapp.com/

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'        : 'H2nf5FWIoLuZWjCdyV21DdAnH',
         'consumerSecret'     : 'TZ7rvD9H1Wh55K3ddPRQfvRrLA6HvabNboGMQadY8QR7zJmjon',
-        'callbackURL'        : 'http://localhost:3000/auth/twitter/callback'
+        'callbackURL'        : 'https://floating-inlet-92048.herokuapp.com/'
     }
 
 };
